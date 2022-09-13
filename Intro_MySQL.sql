9db8c735d21a98e32b64e396d6ed6986d23d1986
Fernanda Duhne <mfduhne@gmail.com>
2022-09-13 17:11:41 +0200

    Intro SQL

---
+++ Intro_MySQL.sql
@@ -0,0 +1,16 @@
+USE sakila;
+-- Instructions: Get all the data from tables actor, film and customer
+SELECT * FROM actor, film, customer;
+-- Get film titles
+SELECT title FROM film;
+-- Get unique list of film languages under the alias language. 
+-- Note that we are not asking you to obtain the language per each film, 
+-- but this is a good time to think about how you might get that information in the future.
+SELECT DISTINCT name FROM language;
+-- 5.1 Find out how many stores does the company have?
+SELECT COUNT(store_id) AS Number_Of_Stores FROM store;
+-- 5.2 Find out how many employees staff does the company have?
+SELECT COUNT(staff_id) AS Number_Of_Staff FROM staff;
+SELECT * FROM staff;
+-- 5.3 Return a list of employee first names only?
+SELECT first_name FROM staff;
